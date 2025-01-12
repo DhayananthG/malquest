62db09c141f17b66b1db6445e2e1f79afd2f83e8
DhayananthG <iamdhayaoffl@gmail.com>
2025-01-12 16:15:36 +0530

    Initialized the Project

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Update paths based on your project
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
